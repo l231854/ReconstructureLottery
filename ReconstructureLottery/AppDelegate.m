d8fc49a313d69914425f9ae9b7b4140818af4489
wangleia <wangleia@xincheng.com>
2018-05-29 17:12:45 +0800

    新的项目

--- ReconstructureLottery/AppDelegate.m
+++ ReconstructureLottery/AppDelegate.m
@@ -98,12 +98,39 @@
     UINavigationController *nav = [[UINavigationController alloc]initWithRootViewController:self.maiTabBarController];
     nav.navigationBarHidden = YES;
     
+    [self showVC];
     self.window.rootViewController =nav;
     
     [self customAppearence];
     [self.window makeKeyAndVisible];
     
     return YES;
+}
+
+#pragma mark -- 根据日期显示
+ -(BOOL)showVC
+{
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    
+    // ----------设置你想要的格式,hh与HH的区别:分别表示12小时制,24小时制
+    
+    [formatter setDateFormat:@"YYYYMMdd"];
+    
+    //现在时间,你可以输出来看下是什么格式
+    
+    NSDate *datenow = [NSDate date];
+    
+    //----------将nsdate按formatter格式转成nsstring
+    
+    NSString *nowtimeStr = [formatter stringFromDate:datenow];
+    NSLog(@"nowtimeStr =  %@",nowtimeStr);
+    BOOL isflage=NO;
+    if ([nowtimeStr compare:@"20180518"]==NSOrderedDescending) {
+        isflage=YES;
+        
+    }
+    
+    return isflage;
 }
 
 #pragma mark-
